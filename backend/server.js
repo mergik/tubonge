3e6ae1517ffbe284272fe03cbf099fc09d30c04c
hillary-254 <123866475+hillary-254@users.noreply.github.com>
2024-03-14 22:24:38 +0300

    DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,11 @@
+//import express
+const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./DB/connectDB')
 dotenv.config()
+const app = express()
+
+app.listen(5000, () => {
+    connectDB()
+    console.log(`listening to port 5000`)
+})
