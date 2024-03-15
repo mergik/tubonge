25266c8fbdada2869b4204f514bca32017f8b15c
KennedyRyan-code <kenryan49@gmail.com>
2024-03-15 13:50:55 +0300

    added middlewares

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./DB/connectDB');
+const AuthRoutes = require('./routes/AuthRoutes');
+
 //import dotenv
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// middleware
+app.use(express.json()); // for parsing application/json (from req.body)
+app.use("/api/auth", AuthRoutes);
+
+app.get('/', (req, res) => {
+    res.send("Welcome to TubongeChat");
+});
 
 // listen to port 5000
 app.listen(PORT, () => {
