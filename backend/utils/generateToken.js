f2a03f849a1a522656cec879940b2934234ee73c
KennedyRyan-code <kenryan49@gmail.com>
2024-03-21 21:56:29 +0300

    jwt token and cookie generater

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const generateTokenandSetCookie = (userId, res) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, { // JWT_SECRET='pQJBVCdnLyVmVngCitHUqRGquEPuoMXJNRwlbUfEIIM='
+        expiresIn: process.env.JWT_EXPIRES_IN // JWT_EXPIRES_IN='15d'
+    })
+
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 1000,
+        httpOnly: true, // prevents cookie from being accessed by client side script
+        sameSite: "strict", // cookie will only be sent in a first-party context
+        secure: process.env.NODE_ENV === 'production' // cookie will only be sent in a HTTPS connection
+    }) 
+}
+
+module.exports = generateTokenandSetCookie;
