a7bc061530090cdee92e5eb826f24d5e535177f2
mergik <gmerlix@yahoo.com>
2024-03-22 20:13:34 +0300

    Created get messages from conversations

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const sendMessage = require('../controllers/messageController');
-const protectRoute = require('../middleware/protectRoute');
+const express = require("express");
+
+const sendMessage = require("../controllers/messageController");
+const getMessages = require("../controllers/messageController");
+const protectRoute = require("../middleware/protectRoute");
 
 const router = express.Router();
 
 // Create a new message
 router.post("/send/:id", protectRoute, sendMessage);
+// Get messages
+router.get("/:id", protectRoute, getMessages);
 
 module.exports = router;
